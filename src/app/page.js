121df21fafeb9d81f24e70a356696b496f516cd4
adalawson96 <adalawson96@gmail.com>
2023-05-28 16:44:53 +0200

    add tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Home(){
     return (
         <main>
-            <h1>hello index page</h1>
+            <h1 className="text-3xl font-bold underline">hello index page</h1>
            <Link href="/dogs">let us see the dogs</Link>
         </main>
     )
